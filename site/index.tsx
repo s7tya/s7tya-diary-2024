198686ff7a6d476fea2334903531b61f73e85916
Shina <53410646+s7tya@users.noreply.github.com>
2024-03-15 05:15:40 +0900

    fix url for gh pages

--- site/index.tsx
+++ site/index.tsx
@@ -8,7 +8,7 @@ export default function () {
                 const paragraphs = (page.data.content as string).split("\n\n");
 
                 return <div>
-                    <a href={page.data.url} style={{ "text-decoration": "none", "color": "black" }}>
+                    <a href={`./posts/${page.data.basename}`} style={{ "text-decoration": "none", "color": "black" }}>
                         <h2>{page.data.basename}</h2>
                         <p>{paragraphs[1].slice(undefined, 80)}{paragraphs[1].length > 80 || paragraphs.length > 2 ? "..." : ""}</p>
                     </a>
