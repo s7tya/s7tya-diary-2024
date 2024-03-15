71ea5c77fe983f676fbb6a2b4f87d2bd49f01fad
Shina <53410646+s7tya@users.noreply.github.com>
2024-03-15 14:39:27 +0900

    update site

--- site/index.tsx
+++ site/index.tsx
@@ -11,7 +11,7 @@ export default function () {
                         <div key={page.data.url} className="post-card">
                             <a href={`./posts/${page.data.basename}`}>
                                 <h2>{page.data.basename}</h2>
-                                <p>{paragraphs[1].slice(undefined, 80)}{paragraphs[1].length > 80 || paragraphs.length > 2 ? "..." : ""}</p>
+                                <p>{paragraphs[1].slice(undefined, 80).replace("\\n", "")}{paragraphs[1].length > 80 || paragraphs.length > 2 ? "..." : ""}</p>
                             </a>
                         </div>
                     )
