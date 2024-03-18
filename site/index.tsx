65779434f7f704fab89b20a728409479c6317fad
s7tya <53410646+s7tya@users.noreply.github.com>
2024-03-18 13:39:56 +0900

    add activity

--- site/index.tsx
+++ site/index.tsx
@@ -1,15 +1,46 @@
 import site from "./_config.ts";
 
+const getDatesBetween = (startDate: Temporal.ZonedDateTime, endDate: Temporal.ZonedDateTime) => {
+  const dateList = [];
+
+  for (let date = startDate; Temporal.ZonedDateTime.compare(date, endDate); date = date.add({ days: 1 })) {
+    const dateObject = new Date(date.toInstant().toString());
+    dateList.push(dateObject);
+  }
+
+  return dateList;
+}
+
 export default function () {
+  const sortedPages = site.pages.filter((page) => page.data.url.startsWith("/posts/")).sort(
+    (a, b) => {
+      return b.data.date.getTime() - a.data.date.getTime();
+    },
+  )
+
+  const sortedPageDates = sortedPages.map((page) => page.data.date.toISOString());
+
+  const todayAtUTC = Temporal.Now.zonedDateTimeISO('Utc').with({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+  const DaysAgoAtUTC = todayAtUTC.subtract({ years: 1 });
+
+  const lastMonthHistory = getDatesBetween(DaysAgoAtUTC, todayAtUTC).map((date) => {
+    return (
+      <div className={`day${sortedPageDates.includes(date.toISOString()) ? " active" : ""}`}>
+      </div>
+    )
+  });
+
+
   return (
     <>
-      <div className="update-checker"></div>
+      <div className="meta">
+        <div className="update-checker"></div>
+        <div className="activity">
+          {lastMonthHistory}
+        </div>
+      </div>
       <div className="post-list">
-        {site.pages.filter((page) => page.data.url.startsWith("/posts/")).sort(
-          (a, b) => {
-            return b.data.date.getTime() - a.data.date.getTime();
-          },
-        ).map(
+        {sortedPages.map(
           (page) => {
             const paragraphs = (page.data.content as string).split("\n\n");
 
