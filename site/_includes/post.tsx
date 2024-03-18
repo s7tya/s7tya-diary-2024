0ad35e5aee2db4b17835440a1a7e83f94d50e274
s7tya <53410646+s7tya@users.noreply.github.com>
2024-03-18 23:17:58 +0900

    前後の記事を表示

--- site/_includes/post.tsx
+++ site/_includes/post.tsx
@@ -1,7 +1,10 @@
 import { Head } from "../_components/Head.tsx";
 import { Header } from "../_components/Header.tsx";
 
-export default ({ title, children, tags }: Lume.Data, helpers: Lume.Helpers) => {
+export default ({ title, children, tags, search, url }: Lume.Data, helpers: Lume.Helpers) => {
+
+    const previousPage = search.previousPage(url, "type=post");
+    const nextPage = search.nextPage(url, "type=post");
 
     return (
         <html>
@@ -15,8 +18,25 @@ export default ({ title, children, tags }: Lume.Data, helpers: Lume.Helpers) => 
                     <aside className="tags">
                         {tags.map((tag) => (<a href={`/tags/${tag}`} className="tag">{tag}</a>))}
                     </aside>
+                    <aside className="pagination">
+                        <div className="previous">
+                            {previousPage && (
+                                <a href={previousPage.url}>
+                                    ← {previousPage.date.toISOString().slice(0, 10).replaceAll("-", ".")}
+                                    {previousPage.title ? ` ${previousPage.title}` : ""}
+                                </a>
+                            )}
+                        </div>
+                        <div className="next">
+                            {nextPage && (
+                                <a href={nextPage.url}>
+                                    {nextPage.date.toISOString().slice(0, 10).replaceAll("-", ".")}
+                                    {nextPage.title ? ` ${nextPage.title}` : ""} →
+                                </a>
+                            )}
+                        </div>
+                    </aside>
                 </div>
-                <script src="/main.js" />
             </body>
         </html>
     );
